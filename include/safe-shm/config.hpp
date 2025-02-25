212375acc9460659ae341481a25fb33f3c62a712
Pavel Guzenfeld <me@pavelguzenfeld.com>
2025-02-25 22:59:52 +0200

    Add initial CMake configuration and header files for safe-shm library

---
+++ include/safe-shm/config.hpp
@@ -0,0 +1,6 @@
+#pragma once
+namespace safe_shm
+{
+    constexpr auto SEM_SUFFIX = "_sem";
+    constexpr auto SEM_INIT = 1;
+} // namespace safe_shm
---
+++ include/safe-shm/storage.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "flat-type/flat.hpp"
+#include "image-shm-dblbuf/image.hpp"
+#include "shm/semaphore.hpp"
+#include "shm/shm.hpp"
+#include "safe-shm/config.hpp"
+
+namespace safe_shm
+{
+    template <FlatType T>
+    class Storage
+    {
+    public:
+        Storage(std::string const &shm_name)
+            : shm_(shm::path(shm_name), sizeof(T)),
+              sem_(shm_name + SEM_SUFFIX, SEM_INIT)
+        {
+        }
+
+        inline void store(T const &data)
+        {
+            sem_.wait();
+            *static_cast<T *>(shm_.get()) = data;
+            sem_.post();
+        }
+
+    private:
+        shm::Shm shm_;
+        shm::Semaphore sem_;
+    };
+} // namespace safe-shm
